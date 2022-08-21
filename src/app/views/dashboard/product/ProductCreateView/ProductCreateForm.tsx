258608d7c02c0bede6d8132eacf53efe4802cad8
algomachine007 <benneth@bejamas.io>
2022-08-21 16:41:45 +0100

    installing notistack and configuring dropzone

--- src/app/views/dashboard/product/ProductCreateView/ProductCreateForm.tsx
+++ src/app/views/dashboard/product/ProductCreateView/ProductCreateForm.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import FilesDropzone from 'app/components/files-dropzone';
 
 const ProductCreateForm = () => {
-  return <div>ProductCreateForm</div>;
+  return (
+    <div>
+      ProductCreateForm Component
+      <FilesDropzone />
+    </div>
+  );
 };
 
 export default ProductCreateForm;
