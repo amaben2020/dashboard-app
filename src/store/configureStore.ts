c08d234b353cd6922f93104ee2b206745a94d6e0
algomachine007 <benneth@bejamas.io>
2022-08-30 20:23:19 +0100

    Cleaning up the project

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -1,42 +1,24 @@
-/**
- * Create the store with dynamic reducers
- */
-
+/*Create the store with dynamic reducers */
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { createInjectorsEnhancer, forceReducerReload } from 'redux-injectors';
-import createSagaMiddleware from 'redux-saga';
-
+import { forceReducerReload } from 'redux-injectors';
 import { createReducer } from './reducers';
-
 export function configureAppStore() {
-  const reduxSagaMonitorOptions = {};
-  const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions);
-  const { run: runSaga } = sagaMiddleware;
-
-  // Create the store with saga middleware
-  const middlewares = [sagaMiddleware];
-
-  const enhancers = [
-    createInjectorsEnhancer({
-      createReducer,
-      runSaga,
-    }),
-  ];
-
   const store = configureStore({
+    /*reducer is required. middleware, devTools, and the rest are optional */
     reducer: createReducer(),
-    middleware: [...getDefaultMiddleware(), ...middlewares],
+    middleware: [
+      ...getDefaultMiddleware({
+        serializableCheck: false,
+      }),
+    ],
     devTools: process.env.NODE_ENV !== 'production',
-    enhancers,
   });
-
-  // Make reducers hot reloadable, see http://mxs.is/googmo
-  /* istanbul ignore next */
+  /* Make reducers hot reloadable, see http://mxs.is/googmo istanbul ignore 
+next */
   if (module.hot) {
     module.hot.accept('./reducers', () => {
       forceReducerReload(store);
     });
   }
-
   return store;
 }
