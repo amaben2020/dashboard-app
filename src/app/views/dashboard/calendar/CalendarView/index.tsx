fae903cd86e9f1ab3dff9c11f16cf9d3bc409884
algomachine007 <benneth@bejamas.io>
2022-09-03 09:06:33 +0100

    creating the addEditEventForm and updating the toolbar component

--- src/app/views/dashboard/calendar/CalendarView/index.tsx
+++ src/app/views/dashboard/calendar/CalendarView/index.tsx
@@ -1,31 +1,173 @@
-import React, { useEffect } from 'react';
-import { getEvents } from 'features/calendar/calendarSlice';
+import React, { useEffect, useState, useRef } from 'react';
+
 import { useDispatch, useSelector } from 'react-redux';
 import { RootState } from 'store/reducers';
+import { Container, makeStyles } from '@material-ui/core';
+import {
+  getEvents,
+  openModal,
+  closeModal,
+  selectRange,
+  selectEvent,
+  updateEvent,
+} from 'features/calendar/calendarSlice';
+import Page from 'app/components/pages';
+import Header from './Header';
+import {
+  Dialog, //a modal popup
+  Paper, //in Material Design, the physical properties of paper are
+  useMediaQuery, // a CSS media query hook for React. Detects when its
+} from '@material-ui/core';
+
+import moment from 'moment';
+import FullCalendar from '@fullcalendar/react';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import listPlugin from '@fullcalendar/list';
+import timelinePlugin from '@fullcalendar/timeline';
+
+import { EventType, ViewType } from 'models/calendar-type';
+import AddEditEventForm from './AddEditEventForm';
 
 const Index = () => {
+  const classes = useStyles();
+
   const dispatch = useDispatch();
 
-  const { events, loading, error } = useSelector(
+  /* destructuring it because we only need the events, loading, error */
+
+  const { events, loading, error, isModalOpen, selectedRange } = useSelector(
     (state: RootState) => state.calendar,
   );
 
   useEffect(() => {
     dispatch(getEvents());
   }, []);
+  const handleAddClick = (): void => {
+    dispatch(openModal());
+  };
+  const handleModalClose = (): void => {
+    dispatch(closeModal());
+  };
+
+  const selectedEventSelector = (state: RootState): EventType | null => {
+    const { events, selectedEventId } = state.calendar;
+    if (selectedEventId) {
+      //@ts-ignore
+      return events?.find(_event => _event.id === selectedEventId);
+    } else {
+      return null;
+    }
+  };
+
+  const selectedEvent = useSelector(selectedEventSelector);
+
   return (
-    <div>
-      <h1>Calendar Works!</h1>
-      {loading && <h2>Loading... </h2>}
-      {error && <h2>Something happened </h2>}
-      <ul>
-        {events?.map(e => (
-          <li key={e.id}>{e.title} </li>
-        ))}
-      </ul>
-    </div>
+    <Container maxWidth={false}>
+      <Header onAddClick={handleAddClick} />
+      <Dialog
+        maxWidth="sm"
+        fullWidth
+        onClose={handleModalClose}
+        open={isModalOpen}
+      >
+        {isModalOpen && (
+          <AddEditEventForm
+            //@ts-ignore
+            event={selectedEvent}
+            range={selectedRange}
+            onAddComplete={handleModalClose}
+            onCancel={handleModalClose}
+            onDeleteComplete={handleModalClose}
+            onEditComplete={handleModalClose}
+          />
+        )}
+      </Dialog>
+    </Container>
   );
 };
 export default Index;
 
 // always use this pattern, have an index file that displays the components and export it
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    minHeight: '100%',
+    paddingTop: theme.spacing(3),
+    paddingBottom: theme.spacing(3),
+  },
+
+  calendar: {
+    marginTop: theme.spacing(3),
+    padding: theme.spacing(2),
+    '& .fc-unthemed .fc-head': {},
+    '& .fc-unthemed .fc-body': {
+      backgroundColor: theme.palette.background.default,
+    },
+    '& .fc-unthemed .fc-row': {
+      borderColor: theme.palette.divider,
+    },
+    '& .fc-unthemed .fc-axis': {
+      ...theme.typography.body2,
+    },
+    '& .fc-unthemed .fc-divider': {
+      borderColor: theme.palette.divider,
+    },
+    '& .fc-unthemed th': {
+      borderColor: theme.palette.divider,
+    },
+
+    '& .fc-unthemed td': {
+      borderColor: theme.palette.divider,
+    },
+    '& .fc-unthemed td.fc-today': {},
+    '& .fc-unthemed .fc-highlight': {},
+    '& .fc-unthemed .fc-event': {
+      backgroundColor: theme.palette.secondary.main,
+      color: theme.palette.secondary.contrastText,
+      borderWidth: 2,
+      opacity: 0.9,
+      '& .fc-time': {
+        ...theme.typography.h6,
+        color: 'inherit',
+      },
+      '& .fc-title': {
+        ...theme.typography.body1,
+        color: 'inherit',
+      },
+    },
+    '& .fc-unthemed .fc-day-top': {
+      ...theme.typography.body2,
+    },
+    '& .fc-unthemed .fc-day-header': {
+      ...theme.typography.subtitle2,
+      fontWeight: theme.typography.fontWeightMedium,
+      color: theme.palette.text.secondary,
+      padding: theme.spacing(1),
+    },
+    '& .fc-unthemed .fc-list-view': {
+      borderColor: theme.palette.divider,
+    },
+    '& .fc-unthemed .fc-list-empty': {
+      ...theme.typography.subtitle1,
+    },
+
+    '& .fc-unthemed .fc-list-heading td': {
+      borderColor: theme.palette.divider,
+    },
+    '& .fc-unthemed .fc-list-heading-main': {
+      ...theme.typography.h6,
+    },
+    '& .fc-unthemed .fc-list-heading-alt': {
+      ...theme.typography.h6,
+    },
+    '& .fc-unthemed .fc-list-item:hover td': {},
+    '& .fc-unthemed .fc-list-item-title': {
+      ...theme.typography.body1,
+    },
+    '& .fc-unthemed .fc-list-item-time': {
+      ...theme.typography.body2,
+    },
+  },
+}));
--- src/features/calendar/calendarSlice.ts
+++ src/features/calendar/calendarSlice.ts
@@ -53,7 +53,8 @@ required.*/
   // actions holds the the auto-generated action creators.
   reducers: {
     setLoading(state, action: PayloadAction<boolean>) {
-      state.loading = action.payload;
+      const { payload } = action;
+      state.loading = payload;
     },
     setError(state, action: PayloadAction<string>) {
       state.error = action.payload;
@@ -93,11 +94,34 @@ the models' folder. */
     },
     closeModal(state) {
       state.isModalOpen = false;
-      state.selectedEventId = null;
+      state.selectedEventId = null || '';
+      //@ts-ignore
       state.selectedRange = null;
     },
   },
 });
+
+/* Export these actions so components can use them. Non-asynchronous 
+actions. HTTP client is not needed.  Think of dispatch as setState()*/
+
+export const selectEvent = (id: string): AppThunk => dispatch => {
+  dispatch(slice.actions.selectEvent(id));
+};
+
+export const selectRange = (start: Date, end: Date): AppThunk => dispatch => {
+  dispatch(
+    slice.actions.selectRange({
+      start: start.getTime(), //getTime() is from moment.js
+      end: end.getTime(),
+    }),
+  );
+};
+export const openModal = (): AppThunk => dispatch => {
+  dispatch(slice.actions.openModal());
+};
+export const closeModal = (): AppThunk => dispatch => {
+  dispatch(slice.actions.closeModal());
+};
 
 // you could do this in another file inside redux services
 /* Asynchronous actions. Actions that require Axios (HTTP client)
@@ -108,7 +132,9 @@ export const getEvents = (): AppThunk => async dispatch => {
   dispatch(slice.actions.setError(''));
   //  {type: 'slice/setError', payload: ''}
   try {
+    /*getting the data */
     const response = await axios.get<EventType[]>(EndPoints.events);
+    /*updating the state in the UI */
     dispatch(slice.actions.getEvents(response.data));
     //  {type: 'slice/getEvents', payload: [{...}]}
 
@@ -121,11 +147,65 @@ export const getEvents = (): AppThunk => async dispatch => {
   }
 };
 
+export const createEvent = (event: EventType): AppThunk => async dispatch => {
+  dispatch(slice.actions.setLoading(true));
+  dispatch(slice.actions.setError(''));
+  try {
+    const { data } = await axios.post<EventType>(EndPoints.events, event);
+    // update the state with that data
+    dispatch(slice.actions.createEvent(data));
+    dispatch(slice.actions.setLoading(false));
+  } catch (error) {
+    dispatch(slice.actions.setError(error.message));
+  }
+};
+
+export const updateEvent = (update: EventType): AppThunk => async dispatch => {
+  dispatch(slice.actions.setLoading(true));
+  dispatch(slice.actions.setError(''));
+  try {
+    const { data } = await axios.put(
+      `${EndPoints.events}/${update.id}`,
+      update,
+    );
+    /*updating the state in the UI */
+    dispatch(slice.actions.updateEvent(data));
+    dispatch(slice.actions.setLoading(false));
+  } catch (error) {
+    dispatch(slice.actions.setError(error.message));
+  }
+};
+
+export const deleteEvent = (id: string): AppThunk => async dispatch => {
+  /*deleting from the database */
+  try {
+    await axios.delete(`${EndPoints.events}/${id}`);
+    dispatch(slice.actions.setLoading(false));
+    dispatch(slice.actions.deleteEvent(id));
+  } catch (error) {
+    dispatch(slice.actions.setError(error.message));
+  }
+
+  /*deleting it from the UI */
+};
+
 // to be used by the store as a slice of state
 export default slice.reducer;
+
+//Remember: you are just using one object for the whole application
 
 // you could use .push in createSlice because it uses immer under the hood
 // ✅ state.push(item)
 // ✅ [...state, item]
+
+//Free resources
 //https://www.codecademy.com/courses/learn-redux/lessons/the-redux-toolkit/exercises/return-object-actions
 // https://www.educative.io/courses/building-teslas-battery-range-calculator-with-react-and-redux/7nVVPYOGVPr
+
+//https://axios-http.com/docs/post_example
+
+// Redux Thunks: Redux Thunk is middleware that allows you to return functions, rather than just actions, within Redux. This allows for delayed actions, including working with promises.
+//it is a redux middleware
+
+// 200: succeeded
+//  201. Created:  The request has been fulfilled and has resulted in one or more new resources being created.
