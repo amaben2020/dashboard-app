c8bb3873cf0dabb93fe105f5c1e917dd940d9f88
algomachine007 <benneth@bejamas.io>
2022-09-10 11:34:20 +0100

    UPDATING PRODUCTION ENVIRONMENT

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,12 @@ import axios from 'axios';
 /*create an instance of axios with a default base URI when sending HTTP 
 requests*/
 /*JSON Server has CORS Policy by default*/
-const api = axios.create({ baseURL: 'http://localhost:5000/' });
+const api = axios.create({
+  baseURL:
+    process.env.NODE_ENV === 'production'
+      ? process.env.REACT_APP_URL
+      : 'http://localhost:5000/',
+});
 export default api;
 export const EndPoints = {
   sales: 'sales',
